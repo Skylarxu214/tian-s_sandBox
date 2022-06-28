024137b406e13fc9a2e2f3c9f19c5ff36e63d6c2
tianxu <xueqingxu0302@gmail.com>
2022-06-27 22:25:39 -0600

    dp

--- sandBox.js
+++ sandBox.js
@@ -40,3 +40,55 @@ function fibonacciIterative(n){
    //  3*factorial(2) = 3*2
    //   2* factoial(1) = 2*1
    //        1(after return 1, bubble up step by steps to n)
+
+
+   var numWays = function(N) {
+    var answer = 0;
+    var recurse = function(number) {
+      if (number === 0) {
+        answer++;
+      } else if (number > 0) {
+        recurse(number - 1);
+        recurse(number - 2);
+        // recurse(number - 3);
+      }
+    };
+    recurse(N);
+    return answer;
+  };
+  
+  /* TEST */
+  
+//   console.log(numWays(1), 1);
+//   console.log(numWays(2), 2);
+//   console.log(numWays(3), 4);
+
+
+function fibPlay(n){
+    let cache = {};
+    return function fib(n){
+        if(n in cache){
+            return cache[n]
+        }else{
+            if(n < 2){
+                return n
+            }else{
+                cache[n] = fib(n-1) + fib(n-2)
+                return cache[n]
+            }
+        }
+
+    }
+
+}
+
+let test = fibPlay()
+
+
+function climbStairs(n){
+    if (n < 2){
+        return n
+    }else if( n > 0){
+        return climbStairs(n -1) + climbStairs(n-2)
+    }
+}
