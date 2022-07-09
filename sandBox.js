f1973e4173fe9e34ab67a3e67dbee12f7311a22b
tianxu <xueqingxu0302@gmail.com>
2022-07-08 22:57:42 -0600

    insertionSort

--- sandBox.js
+++ sandBox.js
@@ -140,3 +140,20 @@ console.log(month.splice(0,0,1)) // before the current index 0 add on 1
 console.log(month)
 console.log(month.splice(2,0,3))// before the current index 2
 console.log(month)
+
+function insertionSort(array) {
+  for(let i =0 ; i< array.length; i++){
+    if (array[i] < array[0]){
+      array.unshift(array.splice(i,1)[0])
+    }else{
+      if(array[i] < array[i -1]){
+        for(let j =1; j< i; j++){
+          if(array[i] > array[j-1] && array[i] < array[j]){
+            array.splice(j,0,array.splice(i,1)[0])
+          }
+        }
+      }
+    }
+  }
+  return array
+}
